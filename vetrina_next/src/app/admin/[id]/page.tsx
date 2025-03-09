30f104fa106182cc324bdd436f75b15654288502
ouerghi01 <mohamedaziz.ouerghi@etudiant-enit.utm.tn>
2025-03-09 22:49:48 +0000

    add vvvxx

--- vetrina_next/src/app/admin/[id]/page.tsx
+++ vetrina_next/src/app/admin/[id]/page.tsx
@@ -51,7 +51,7 @@ export default async function Page({
 }
 
 
-export  async function RenderRequests(requests: {
+async function RenderRequests(requests: {
   id: string;
   user: { email: string } | null;
   product: { name: string } | null;
